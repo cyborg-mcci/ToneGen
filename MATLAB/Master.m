130a44fbf722ccd516e4a83a206f52861cc52a52
BrendanOCall <brendan.ocallaghan@mcci.ie>
2019-06-20 17:33:40 +0100

    Update with calculation to find nearest power of 2 to length of ST_Array

--- MATLAB/Master.m
+++ MATLAB/Master.m
@@ -1,7 +1,7 @@
 sample_cycle_ratio = 1000;
 Full_Scale = 2;
 num_bits = 5;
-f_in = 1000;
+f_in = 0.5;
 
 [Dig_Out,Time_Out] = ADC(sample_cycle_ratio,Full_Scale,num_bits);
 Normalised_time = Time_Out.*(1/f_in); % normalising time series
@@ -11,8 +11,11 @@ plot(Stitched_TArray,Stitched_DArray);
 
 fs = sample_cycle_ratio * f_in;
 OSR=1;
+L = length(Stitched_TArray);
+y = log2(L);
+N = floor(y);
 
-[snr, enob, pot_signal_B, f, PSD] = gs_fresp(Stitched_TArray, 8192, fs, fi, OSR);
+[snr, enob, pot_signal_B, f, PSD] = gs_fresp(Stitched_TArray, 2^N, fs, fi, OSR);
 
 
 
