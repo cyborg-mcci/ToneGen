a4f0dcacde31f222395072b2c53555142e221574
anthonyjwall <anthonywall@me.com>
2019-06-26 14:27:43 +0100

    Revert "fixed FFT"
    This reverts commit baa07cb1eb12b984ab075146770e3365495aa31f.

--- MATLAB/ADC.m
+++ MATLAB/ADC.m
@@ -1,11 +1,10 @@
 function [DigitalOutput,sample_times] = ADC(sample_cycle_ratio,Full_Scale,num_bits)
 
-MC=41;
 samples = sample_cycle_ratio; 
-k = 0:samples:MC-samples; % sample points
+k = linspace(0,1,samples); % sample points
 FS = 2^num_bits;
 
-D = zeros(1,length(k));
+D = zeros(1,samples);
 
 %Storing sampled values in I array
 
