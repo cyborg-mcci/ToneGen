9d58ae51fe0eab6be30b1c595d17fbc7a42d6c6d
BrendanOCall <brendan.ocallaghan@mcci.ie>
2019-06-26 14:51:07 +0100

    Update Testing.m

--- MATLAB/Testing.m
+++ MATLAB/Testing.m
@@ -33,7 +33,29 @@ plot(t,y)
 plot(t,R)
 
 
++% f_in = 1e2;
+% [i,t] = gensig('sin',1/f_in);
+% 
+% R = 1e3;
+% C = 1e-7; %fc = 1.6kHz
+% TFnum = [1];
+% TFden = [R*C 1];
+% TF = tf(TFnum, TFden);
+% y = Filtering(TFnum, TFden, i, t);
+% plot(t,i);
+% hold on
+% plot(t,y);
+% grid on
 
+% f_in = 1e1;
+% [i,t] = gensig('sin',1/f_in);
+% th_noise = 5*rand(*ones(1,length(t));
+% th_noise_sig = i+th_noise;
+% plot(t,th_noise_sig);
+
+
+k = physconst('Boltzmann');
+T = 293.15;
 
 
 
