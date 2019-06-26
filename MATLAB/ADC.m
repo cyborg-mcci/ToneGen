baa07cb1eb12b984ab075146770e3365495aa31f
anthonyjwall <anthonywall@me.com>
2019-06-26 13:16:54 +0100

    fixed FFT

--- MATLAB/ADC.m
+++ MATLAB/ADC.m
@@ -1,10 +1,11 @@
 function [DigitalOutput,sample_times] = ADC(sample_cycle_ratio,Full_Scale,num_bits)
 
+MC=41;
 samples = sample_cycle_ratio; 
-k = linspace(0,1,samples); % sample points
+k = 0:samples:MC-samples; % sample points
 FS = 2^num_bits;
 
-D = zeros(1,samples);
+D = zeros(1,length(k));
 
 %Storing sampled values in I array
 
--- MATLAB/Master.m
+++ MATLAB/Master.m
@@ -35,35 +35,41 @@ FigureCounter = 0;
 maxNumCompThreads(12);
 
 %% Parameter Declaration
-sample_cycle_ratio = 739;
+MC=41;
+N=2048;
+sample_cycle_ratio = MC/N;
 Full_Scale = 2;
-num_bits = 12;
+num_bits = 16;
 f_in = 1e6;
 
 [Dig_Out,Time_Out] = ADC(sample_cycle_ratio,Full_Scale,num_bits);
 Normalised_time = Time_Out.*(1/f_in); % normalising time series
 
-[Stitched_DArray,Stitched_TArray] = StitchedArray(Dig_Out,Normalised_time);
-FigureCounter = FigureCounter + 1;
-figure(FigureCounter)
-plot(Stitched_TArray,Stitched_DArray);
-grid on
+% [Stitched_DArray,Stitched_TArray] = StitchedArray(Dig_Out,Normalised_time);
+% FigureCounter = FigureCounter + 1;
+% figure(FigureCounter)
+% plot(Stitched_TArray,Stitched_DArray);
+% grid on
 
 
+% OSR=1;
+% L = length(Stitched_TArray);
+% y = log2(L);
+% N = floor(y);
+% 
+% M = 59;
+% cycinsample = Stitched_TArray(end)*f_in;
+% numdatapts = length(Stitched_TArray);
+% fs = f_in*2^N/41; %%%%%%%% fi/fs = #cyclesperwindow/#dataptsperwindow
+% 
+% SDA = Stitched_DArray(1:2^N)';
+
+fs=f_in/sample_cycle_ratio;
 OSR=1;
-L = length(Stitched_TArray);
-y = log2(L);
-N = floor(y);
 
-M = 59;
-cycinsample = Stitched_TArray(end)*f_in;
-numdatapts = length(Stitched_TArray);
-fs = f_in*2^N/41; %%%%%%%% fi/fs = #cyclesperwindow/#dataptsperwindow
+[snr, enob, pot_signal_B, f, PSD] = gs_fresp(Dig_Out', N, fs, f_in, OSR);
 
-SDA = Stitched_DArray(1:2^N)';
-
-[snr, enob, pot_signal_B, f, PSD] = gs_fresp(SDA, 2^N, fs, fi, OSR);
-
+enob
 % num = [5];
 % den = [1];
 % t = Stitched_TArray(1:length(Stitched_DArray));
