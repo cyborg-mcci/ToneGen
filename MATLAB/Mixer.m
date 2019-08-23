c854e6c95d10727dcb500551e578004b04275b2d
BrendanOCall <brendan.ocallaghan@mcci.ie>
2019-08-23 10:29:24 +0100

    Revert "TIA_Converter w/ block comments"
    This reverts commit 248e1cbc3eda06cda44847aa499e6247b9552d4d.

--- MATLAB/Mixer.m
+++ MATLAB/Mixer.m
@@ -1,16 +1,6 @@
 function Mixer_Output = Mixer(Channel_1,Channel_2,TNoiseMixer,FCornerMixer,fs)
-%%%% A function which multiplies two signals together and returns the
-%%%% product 
-
-%%%% INPUT PARAMETERS
-%%% Channel_1 - the first input channel to the mixer
-%%% Channel_2 - the second input channel to the mixer
-%%% TNoiseMixer - the mixer thermal noise (output-referred by default)
-%%% FCornerMixer - the mixer 1/f noise corner frequency (unit = Hz)
-%%% fs - the DAC sampling frequency
-
-%%%%OUTPUT PARAMETERS
-%%% Mixer_Output - the multplied signal
+%%%%Returns output of DAC (in Amps)
+%%%%TNoiseDAC in A/sqrt(Hz)
 
 Product = Channel_1.*Channel_2;
 
@@ -21,6 +11,6 @@ var_Flicker = FCornerMixer*(TNoiseMixer^2)*log(length(Product)/2);
 [FlickerMixer,~] = f_alpha(length(Product),var_Flicker,1,1);
 
 Mixer_Noise = Thermal + FlickerMixer';
-Mixer_Output = Product + Mixer_Noise;
-
+%Mixer_Output = Product + Mixer_Noise;
+Mixer_Output = Product;
 end
--- MATLAB/TIA_Converter.m
+++ MATLAB/TIA_Converter.m
@@ -1,16 +1,4 @@
 function [DAC_Output_v] = TIA_Converter(Signal,Converter_R,Temperature,Op_Amp_Noise)
-%%%% A function which converts the current output of the DAC to a voltage
-%%%% signal
-
-%%%% INPUT PARAMETERS
-%%% Signal - the current signal to be converted (unit = A)
-%%% Converter_R - the TIA resistor (unit = Ohms)
-%%% Temperature - the temperature (unit = K)
-%%% Op_Amp_Noise - the input-referred op amp noise voltage density
-%%% (unit = V/sqrt(Hz))
-
-%%%% OUTPUT PARAMETERS
-%%% DAC_Output_v - the DAC output converted to a voltage signal 
 
 global k;
 Resistor_Noise = randn(1,length(Signal))*sqrt(4*k*Temperature*Converter_R);
