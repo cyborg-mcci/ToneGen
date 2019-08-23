248e1cbc3eda06cda44847aa499e6247b9552d4d
BrendanOCall <brendan.ocallaghan@mcci.ie>
2019-08-23 10:28:15 +0100

    TIA_Converter w/ block comments

--- MATLAB/Mixer.m
+++ MATLAB/Mixer.m
@@ -1,6 +1,16 @@
 function Mixer_Output = Mixer(Channel_1,Channel_2,TNoiseMixer,FCornerMixer,fs)
-%%%%Returns output of DAC (in Amps)
-%%%%TNoiseDAC in A/sqrt(Hz)
+%%%% A function which multiplies two signals together and returns the
+%%%% product 
+
+%%%% INPUT PARAMETERS
+%%% Channel_1 - the first input channel to the mixer
+%%% Channel_2 - the second input channel to the mixer
+%%% TNoiseMixer - the mixer thermal noise (output-referred by default)
+%%% FCornerMixer - the mixer 1/f noise corner frequency (unit = Hz)
+%%% fs - the DAC sampling frequency
+
+%%%%OUTPUT PARAMETERS
+%%% Mixer_Output - the multplied signal
 
 Product = Channel_1.*Channel_2;
 
@@ -11,6 +21,6 @@ var_Flicker = FCornerMixer*(TNoiseMixer^2)*log(length(Product)/2);
 [FlickerMixer,~] = f_alpha(length(Product),var_Flicker,1,1);
 
 Mixer_Noise = Thermal + FlickerMixer';
-%Mixer_Output = Product + Mixer_Noise;
-Mixer_Output = Product;
+Mixer_Output = Product + Mixer_Noise;
+
 end
